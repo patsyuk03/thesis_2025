fixed mean/std calculation and added single traj visualization

ur5_mjx_planner/mjx_planner.py
@@ -8,7 +8,7 @@ import numpy as np
 import time
 
 class MJXPlanner:
-    def __init__(self, model_path, n_batch=3, n_samples=3, n_steps=500, visualize=False):
+    def __init__(self, model_path, n_batch=3, n_samples=10, n_steps=100, visualize=False):
         print("Initializing planner...")
 
         self.model_path = model_path
@@ -44,7 +44,7 @@ class MJXPlanner:
 
         # self.prev_time = self.data.time
         # while self.visualize:
-        #     self.visualization(camera)
+        #     self.visualization()
 
     def generate_samples(self):
         samples = np.zeros((self.n_samples, self.n_steps, self.n_joints), float)
@@ -62,14 +62,11 @@ class MJXPlanner:
             temp_mjx_data = self.jit_step(self.mjx_model, temp_mjx_data)
             current_position = temp_mjx_data.xpos[self.model.body(name="hande").id]
             total_cost += jnp.linalg.norm(self.target_position - current_position)
-            if self.visualize and idx>1:
-                # print(total_cost.shape, total_cost)
-                # print(temp_mjx_data.shape)
-                self.visualization(temp_mjx_data)
 
             if idx%100 == 0:
                 print(f"Total cost at step #{idx}: {total_cost}")
                 print("Current pose",current_position)
+
         return total_cost
 
     def evaluate_batch(self, batch_trajectories):
@@ -78,38 +75,41 @@ class MJXPlanner:
 
 
     def optimizer(self):
+        avg_batch_cost = list()
         for batch in range(self.n_batch):
             samples = self.generate_samples()
 
             costs = self.evaluate_batch(samples)
             
-            elite_idxs = np.argsort(costs)[:self.n_samples // 2]
+            elite_idxs = np.argsort(costs)[:self.n_samples // 5]
             elite_samples = samples[elite_idxs]
+            elite_samples = elite_samples.reshape((elite_samples.shape[0]*elite_samples.shape[1], elite_samples.shape[2]))
 
             self.mean = np.mean(elite_samples, axis=0)
             self.std = np.std(elite_samples, axis=0)
 
-            print(f"Batch {batch + 1}/{self.n_batch}: Mean cost = {np.mean(costs):.4f}")
+            print(f"Batch {batch + 1}/{self.n_batch}: Mean cost = {np.mean(costs):.4f} \nMean: {self.mean}\nSTD: {self.std}")
 
-    def visualization(self, traced_data):
-        # if self.mjx_data.time - self.prev_time >= 1/fps:
-        #     self.prev_time = self.data.time
+            avg_batch_cost.append(np.mean(costs))
+        return avg_batch_cost
+    
+    def run_trajectory(self, mean=None, std=None):
+        if mean!=None and std!=None:
+            self.mean = mean
+            self.std = std
+        samples = self.generate_samples()
+        traj = 0
+        while self.visualize:
+            for vel in samples[traj]:
+                self.data.qvel[:self.n_joints] = vel
+                mujoco.mj_step(self.model, self.data)
+                out = self.visualization(traj)
+                if traj != out or not self.visualize:
+                    traj = out
+                    break                
 
-        # print(mjx_data)
-
-        # datas = jax.vmap(lambda _data: mjx.get_data(self.model, _data))(mjx_data)
-
-
-        # image = self.renderer.render()
-        # for idx, data in enumerate(datas):
-        #     self.renderer.update_scene(data, camera=self.camera)
-        #     temp_image = self.renderer.render()
-        #     image = np.concatenate((image, temp_image), axis=1)
-
-        mjx_data = jax.lax.stop_gradient(traced_data)
-
-        data = mjx.get_data(self.model, mjx_data)
-        self.renderer.update_scene(data, camera=self.camera)
+    def visualization(self, traj):
+        self.renderer.update_scene(self.data, camera=self.camera)
         image = self.renderer.render()
 
 
@@ -118,6 +118,20 @@ class MJXPlanner:
         if key == ord("q"):
             self.visualize = False
             cv2.destroyAllWindows()
+        elif key == ord("w"):
+            if traj < self.n_samples:
+                traj += 1
+            else:
+                traj = 0
+            print("Traj:", traj)
+        elif key == ord("s"):
+            if traj > 0:
+                traj -= 1
+            else:
+                traj = self.n_samples-1
+            print("Traj:", traj)
+
+        return traj
 
         
 
@@ -129,8 +143,18 @@ class MJXPlanner:
 
 def main():
     model_path = f"{os.path.dirname(__file__)}/../universal_robots_ur5e/scene_mjx.xml" 
-    mp = MJXPlanner(model_path, visualize=True)
-    mp.optimizer()
+    mp = MJXPlanner(model_path, n_batch=10, n_samples=10, n_steps=500, visualize=False)
+
+    # mean = [0.57053006,  0.1621206,  -0.00401804, -0.01125557, -0.00737564, -0.20637755, 0. ]
+    # std = [1.0005847,  1.0102792,  0.9040343,  0.9934344,  0.96778953, 1.0823556, 0. ]
+    # mp.run_trajectory(mean=mean, std=std)
+
+    cost_list = mp.optimizer()
+    file_path = f"{os.path.dirname(__file__)}/costs.txt" 
+    with open(file_path, 'w') as file:
+        for cost in cost_list:
+            file.write(f'{cost}\n')
+
 
 
 