ae56011826ebd8df32450f6829ef2a844ad0c6b4
Iryna Hurova <iryna.gurova@gmail.com>
2025-02-18 12:37:00 +0200

    testing

--- mjx_planner_v2/plot_cost.py
+++ mjx_planner_v2/plot_cost.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-file_path = f"{os.path.dirname(__file__)}/cost_g_best.csv" 
+# file_path = f"{os.path.dirname(__file__)}/cost_g_best.csv" 
+file_path = f"{os.path.dirname(__file__)}/output_costs1.csv" 
+
 
 costs = np.genfromtxt(file_path, delimiter=',')
 
 print(costs)
 
 plt.plot(costs)
-plt.savefig('costs1.png')
+plt.savefig('costs.png')
