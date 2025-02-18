1b7a23ed7b4ecd952314d26b43d4a57e68689d5e
Iryna Hurova <iryna.gurova@gmail.com>
2025-02-18 12:52:42 +0200

    -

--- mjx_planner_v2/plot_cost.py
+++ mjx_planner_v2/plot_cost.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-# file_path = f"{os.path.dirname(__file__)}/cost_g_best.csv" 
-file_path = f"{os.path.dirname(__file__)}/output_costs1.csv" 
+file_path = f"{os.path.dirname(__file__)}/cost_g_best.csv" 
+# file_path = f"{os.path.dirname(__file__)}/output_costs1.csv" 
 
 
 costs = np.genfromtxt(file_path, delimiter=',')
