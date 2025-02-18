a209798bacbe6b2f1761cf5546df39d9d5d866e9
Iryna Hurova <iryna.gurova@gmail.com>
2025-02-18 12:30:33 +0200

    modified cost calculation

--- mjx_planner_v2/plot_cost.py
+++ mjx_planner_v2/plot_cost.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-file_path = f"{os.path.dirname(__file__)}/output_costs1.csv" 
+file_path = f"{os.path.dirname(__file__)}/cost_g_best.csv" 
 
 costs = np.genfromtxt(file_path, delimiter=',')
 
--- mjx_planner_v2/test.py
+++ mjx_planner_v2/test.py
@@ -1,12 +1,5 @@
 import numpy as np
 
-a = np.arange(3)
-a = np.tile(a, (10, 1))
+a = np.linspace(0, 1, 10)
 
-b = np.array([4, 5, 6])
-b = np.tile(b, (10, 1))
-
-
-c = np.linalg.norm(a - b, axis = 1)
-
-print(c)
+print(a)
