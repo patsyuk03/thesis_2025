807ad09c60bc88f9997d092f2fe3810c91f36b23
Iryna Hurova <iryna.gurova@gmail.com>
2025-02-21 18:15:00 +0200

    testing collisions

--- mjx_planner_v2/view_traj.py
+++ mjx_planner_v2/view_traj.py
@@ -12,19 +12,22 @@ model.opt.timestep = 0.01
 data = mujoco.MjData(model)
 
 init_joint_state = [1.5, -1.8, 1.75, -1.25, -1.6, 0]
-# init_joint_state = [0.7, -0.8, 0, 0, 0, 0]
+# init_joint_state = [1.3, -0.8, 0, 0, 0, 0]
 data.qpos[:6] = init_joint_state
 # data.ctrl[:6] = data.qpos[:6]
 
 file_path = f"{os.path.dirname(__file__)}/best_vels.csv" 
 thetadot = np.genfromtxt(file_path, delimiter=',').T
-# print(thetadot.shape)
-# thetadot = np.tile(np.array([0, 0.1, 0, 0, 0, 0]), (200, 1))
 # thetadot = np.tile(np.zeros(6), (200, 1))
+# thetadot[:, 1] = 0.5
+
+no_contact = np.zeros(4)
 
 
 with viewer.launch_passive(model, data) as viewer_:
     viewer_.cam.distance = 4
+    viewer_.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = True
+
     start = time.time()
     i = 0
     while viewer_.is_running():
@@ -36,6 +39,10 @@ with viewer.launch_passive(model, data) as viewer_:
         time_until_next_step = model.opt.timestep - (time.time() - step_start)
         if time_until_next_step > 0:
             time.sleep(time_until_next_step)
+
+        if len(data.contact.geom1) != len(no_contact):
+            print(f"collision at step: {i}")
+        # print(len(data.contact.geom2), len(no_contact))
 
         if i < thetadot.shape[0]-1:
             i+=1
