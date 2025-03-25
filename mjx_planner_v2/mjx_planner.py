fixed rotation cost

mjx_planner_v2/mjx_planner.py
@@ -103,11 +103,10 @@ class cem_planner():
 		self.table_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_GEOM, 'table_geom_1')
 		self.fill_value = int((np.array(self.mjx_data.contact.geom.tolist()) == (self.table_id,self.object_0_id)).all(axis=1).nonzero()[0][0])
 
-		object_pos = self.model.body(name="object_0").pos
-		object_pos[-1] += 0.3
-		self.target_pos = np.tile(object_pos, (self.num, 1))
+		self.target_pos = self.model.body(name="object_0").pos
+		self.target_pos[-1] += 0.3
 
-		self.target_rot = np.array([0, 0, -1])
+		self.target_rot = self.quaternion_multiply(self.mjx_data.xquat[self.model.body(name="object_0").id], [0, 1, 0, 0])
 
 		self.hande_id = self.model.body(name="hande").id
 
@@ -122,13 +121,27 @@ class cem_planner():
 			f'\n Default backend: {jax.default_backend()}'
 			f'\n Model path: {self.model_path}',
 			f'\n Timestep: {self.t}',
-			f'\n Target position: {self.target_pos[0]}',
+			f'\n Target position: {self.target_pos}',
+			f'\n Target orientation: {self.target_rot}',
 			f'\n CEM Iter: {self.maxiter_cem}',
 			f'\n Number of batches: {self.num_batch}',
 			f'\n Number of steps per trajectory: {self.num}',
 			f'\n Time per trajectory: {self.t_fin}',
 		)
-	
+
+		
+	def quaternion_multiply(self, q1, q2):
+		"""Multiply two quaternions q1 * q2"""
+		w1, x1, y1, z1 = q1
+		w2, x2, y2, z2 = q2
+		
+		w = w2 * w1 - x2 * x1 - y2 * y1 - z2 * z1
+		x = w2 * x1 + x2 * w1 + y2 * z1 - z2 * y1
+		y = w2 * y1 - x2 * z1 + y2 * w1 + z2 * x1
+		z = w2 * z1 + x2 * y1 - y2 * x1 + z2 * w1
+		
+		return jnp.array([w, x, y, z])
+		
   
 	def get_A_traj(self):
 		A_theta = np.kron(np.identity(self.num_dof), self.P )
@@ -238,31 +251,15 @@ class cem_planner():
 			primal_sol, s_v, s_a, s_p,  lamda_v, lamda_a, lamda_p, res_projection  = self.compute_projection(lamda_v, lamda_a, lamda_p, s_v, s_a, s_p,b_eq_term,  xi_samples)
 	 
 		return primal_sol
-	
-	@partial(jax.jit, static_argnums=(0,))
-	def quaternion_to_euler(self, quaternion):
-		w, x, y, z = quaternion
-		ysqr = y * y
-		t0 = +2.0 * (w * x + y * z)
-		t1 = +1.0 - 2.0 * (x * x + ysqr)
-		X = jnp.rad2deg(jnp.arctan2(t0, t1))
-		t2 = +2.0 * (w * y - z * x)
-		t2 = jnp.clip(t2, a_min=-1.0, a_max=1.0)
-		Y = jnp.rad2deg(jnp.arcsin(t2))
-		t3 = +2.0 * (w * z + x * y)
-		t4 = +1.0 - 2.0 * (ysqr + z * z)
-		Z = jnp.rad2deg(jnp.arctan2(t3, t4))
-		euler = jnp.round(jnp.array([X,Y,Z]), 2)
-		return euler
 
-	@partial(jax.jit, static_argnums=(0,))
+	
 	def mjx_step(self, mjx_data, thetadot_single):
 		qvel = mjx_data.qvel.at[:self.num_dof].set(thetadot_single)
 		mjx_data = mjx_data.replace(qvel=qvel)
 		mjx_data = self.jit_step(self.mjx_model, mjx_data)
 		theta = mjx_data.qpos[:self.num_dof]
 		eef_pos = mjx_data.xpos[self.hande_id]
-		eef_rot = mjx_data.xmat[self.hande_id][2]
+		eef_rot = mjx_data.xquat[self.hande_id]
 		collision = mjx_data.contact.geom[jnp.where(mjx_data.contact.dist<0, size=100, fill_value=self.fill_value)].flatten()
 
 		return mjx_data, (theta, eef_pos, eef_rot, collision)
@@ -283,7 +280,9 @@ class cem_planner():
 		cost_g_ = jnp.linalg.norm(eef_pos - self.target_pos, axis=1)
 		cost_g = cost_g_[-1] + jnp.sum(cost_g_[:-1])*1
 
-		cost_r_ = jnp.linalg.norm(eef_rot - self.target_rot, axis=1)
+		dot_product = jnp.abs(jnp.dot(eef_rot/jnp.linalg.norm(eef_rot, axis=1).reshape(1, self.num).T, self.target_rot/jnp.linalg.norm(self.target_rot)))
+		dot_product = jnp.clip(dot_product, -1.0, 1.0)
+		cost_r_ = 2 * jnp.arccos(dot_product)
 		cost_r = cost_r_[-1] + jnp.sum(cost_r_[:-1])*1
 
 		cost_c = jnp.sum(jnp.isin(self.geom_ids, collision))

mjx_planner_v2/mpc_planner.py
@@ -9,23 +9,9 @@ import os
 from mujoco import viewer
 import matplotlib.pyplot as plt
 
-# def update_plot(fig, axs, vels_plot, cost_plot, vels, costs, steps):
-#     for i, joint in enumerate(vels_plot):
-#         joint.set_data(steps, vels[i])
-
-#     cost_plot.set_data(steps, costs)
-
-#     # Set plot limits
-#     # self.ax1.set_xlim([max(0, len(self.time_stamps) - 100), len(self.time_stamps)])
-#     # self.ax2.set_xlim([max(0, len(self.time_stamps) - 100), len(self.time_stamps)])
-
-#     # Draw the updated plot
-#     axs[0].relim()
-#     axs[1].relim()
-#     axs[0].autoscale_view(True, True, True)
-#     axs[1].autoscale_view(True, True, True)
-#     fig.canvas.draw()
-#     fig.canvas.flush_events()
+def quaternion_distance(q1, q2):
+    dot_product = np.abs(np.dot(q1, q2))
+    return 2 * np.arccos(dot_product)
 
 start_time = time.time()
 cem =  cem_planner(
@@ -35,7 +21,7 @@ cem =  cem_planner(
     maxiter_cem=5,
     w_pos=5,
     w_rot=2,
-    w_col=100,
+    w_col=10,
     num_elite=0.01,
     timestep=0.04
     )
@@ -53,28 +39,6 @@ print(f"Compute CEM: {round(time.time()-start_time, 2)}s")
 
 thetadot = np.array([0]*6)
 
-# plt.ion()
-# fig, axs = plt.subplots(2, 1, figsize=(12, 6))
-# axs[0].set_title('Cost')
-# axs[0].set_ylabel('Cost')
-# axs[1].set_title('Velocities')
-# axs[1].set_ylabel('Velocity')
-# axs[1].set_xlabel('Step')
-
-# vel_plot = [axs[1].plot([], [], label=f'Joint {joint}')[0] for joint in np.arange(1,7)]
-# cost_plot = axs[0].plot([], [])[0]
-
-# axs[0].legend()
-# axs[1].legend()
-
-# vels = list()
-# costs = list()
-# steps = list()
-
-# vels.append(thetadot)
-# costs.append(np.linalg.norm(data.xpos[cem.hande_id] - cem.target_pos))
-# steps.append(len(steps))
-
 with viewer.launch_passive(model, data) as viewer_:
     viewer_.cam.distance = 4
     viewer_.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = True
@@ -88,15 +52,10 @@ with viewer.launch_passive(model, data) as viewer_:
         mujoco.mj_step(model, data)
 
         cost_g = np.linalg.norm(data.xpos[cem.hande_id] - cem.target_pos)   
-        cost_r = np.linalg.norm(data.xmat[cem.hande_id][-3:] - cem.target_rot)  
-        print(f'Step Time: {round(time.time() - start_time, 2)}s | Cost g: {round(float(cost_g), 2)} | Cost r: {round(float(cost_r), 2)}')
+        cost_r = quaternion_distance(data.xquat[cem.hande_id], cem.target_rot)  
+        print(f'Step Time: {"%.0f"%((time.time() - start_time)*1000)}ms | Cost g: {"%.2f"%(float(cost_g))} | Cost r: {"%.2f"%(float(cost_r))} | Cost: {cost}')
 
         viewer_.sync()
-
-        # vels.append(thetadot)
-        # costs.append(cost_)
-        # steps.append(len(steps))
-        # update_plot(fig, axs, vel_plot, cost_plot, np.transpose(vels), costs, steps)
 
         time_until_next_step = model.opt.timestep - (time.time() - start_time)
         if time_until_next_step > 0:

mjx_planner_v2/test.py
@@ -1,23 +1,19 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 
-# Create the figure and axis
-fig, ax = plt.subplots()
-xdata, ydata = [], []
-line, = ax.plot([], [], 'r-')
+def quaternion_distance(q1, q2):
 
-# Initialize the plot
-ax.set_xlim(0, 10)
-ax.set_ylim(-1, 1)
+    dot_product = np.abs(np.dot(q1/np.linalg.norm(q1, axis=1).reshape(1, 5).T, q2/np.linalg.norm(q2)))
+    print(q1.shape, np.linalg.norm(q1, axis=1).T.shape)
+    return 2 * np.arccos(dot_product)
 
-def update(frame):
-    xdata.append(frame / 10)
-    ydata.append(np.sin(frame / 10 * 2 * np.pi))
-    line.set_data(xdata, ydata)
-    return line,
 
-# Create the animation
-ani = FuncAnimation(fig, update, frames=range(100), blit=True)
 
-plt.show()
+q1 = np.tile(np.array([1, 0, 1, 0]), (5, 1))
+# q1 = np.array([1, 0, 1, 0], dtype=float)
+q2 = np.array([1, 0, 1, 0], dtype=float)
+
+# print(q1)
+
+
+a = quaternion_distance(q1, q2)
+print(a)

mjx_planner_v2/view_traj.py
@@ -6,42 +6,9 @@ import numpy as np
 import time
 import jax
 
-# def quaternion_to_euler(quaternion):
-#     w, x, y, z = quaternion
-#     ysqr = y * y
-
-#     t0 = +2.0 * (w * x + y * z)
-#     t1 = +1.0 - 2.0 * (x * x + ysqr)
-#     X = np.rad2deg(np.arctan2(t0, t1))
-
-#     t2 = +2.0 * (w * y - z * x)
-
-#     t2 = np.clip(t2, a_min=-1.0, a_max=1.0)
-#     Y = np.rad2deg(np.arcsin(t2))
-
-#     t3 = +2.0 * (w * z + x * y)
-#     t4 = +1.0 - 2.0 * (ysqr + z * z)
-#     Z = np.rad2deg(np.arctan2(t3, t4))
-
-#     euler = np.round(np.array([X,Y,Z]), 2)
-
-#     return euler
-
-def log_map_rotation_matrix(R):
-    theta = np.arccos(np.clip((np.trace(R) - 1) / 2, -1.0, 1.0))
-    if np.isclose(theta, 0):
-        return np.zeros((3, 3))
-    elif np.isclose(theta, np.pi):
-        w = np.sqrt((np.diag(R) + 1) / 2)
-        w[np.isnan(w)] = 0
-        skew_w = np.array([
-            [0, -w[2], w[1]],
-            [w[2], 0, -w[0]],
-            [-w[1], w[0], 0]
-        ])
-        return np.pi * skew_w
-    else:
-        return (theta / (2 * np.sin(theta))) * (R - R.T)
+def quaternion_distance(q1, q2):
+    dot_product = np.abs(np.dot(q1, q2))
+    return 2 * np.arccos(dot_product)
 
 
 model_path = f"{os.path.dirname(__file__)}/ur5e_hande_mjx/scene.xml" 
@@ -53,7 +20,7 @@ init_joint_state = [1.5, -1.8, 1.75, -1.25, -1.6, 0]
 # init_joint_state = [-1.07386628, -2.21057648, -2.35288999, -1.76991373, -1.09578535, -2.99189036]
 # init_joint_state = [1.3, -0.8, 0, 0, 0, 0]
 data.qpos[:6] = init_joint_state
-# data.ctrl[:6] = init_joint_state
+data.ctrl[:6] = init_joint_state
 
 mjx_model = mjx.put_model(model)
 mjx_data = mjx.put_data(model, data)
@@ -76,8 +43,8 @@ with viewer.launch_passive(model, data) as viewer_:
     i = 0
     while viewer_.is_running():
         step_start = time.time()
-        data.qvel[:6] = thetadot[i]
-        # data.qpos[:6] = data.ctrl[:6]
+        # data.qvel[:6] = thetadot[i]
+        data.qpos[:6] = data.ctrl[:6]
 
         # quat_gripper = data.xquat[model.body(name="hande").id]
 
@@ -86,11 +53,11 @@ with viewer.launch_passive(model, data) as viewer_:
         # mjx_data = jit_step(mjx_model, mjx_data)
 
         rot_matrix = data.xmat[model.body(name="hande").id].reshape(3,3)
-        print(mjx_data.xmat[model.body(name="hande").id].shape)
+        # print(mjx_data.xmat[model.body(name="hande").id].shape)
         # rot_matrix = log_map_rotation_matrix(rot_matrix)
         rot_matrix = np.round(rot_matrix, 1)
         # rot_matrix = data.xmat[model.body(name="wrist_3_link_1").id].reshape(3,3).T @ rot_matrix
-        print(mjx_data.xmat[model.body(name="hande").id])
+        print(rot_matrix)
 
         mujoco.mj_step(model, data)
         viewer_.sync()