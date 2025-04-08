733f861d820e04cefd4c12e16d302eba05b0c990
Iryna Hurova <iryna.gurova@gmail.com>
2025-04-08 13:14:02 +0300

    -

--- sampling_based_planner/view_model.py
+++ sampling_based_planner/view_model.py
@@ -10,8 +10,11 @@ import numpy as np
 model_path = f"{os.path.dirname(__file__)}/ur5e_hande_mjx/scene.xml" 
 model = mujoco.MjModel.from_xml_path(model_path)
 data = mujoco.MjData(model)
-mjx_model = mjx.put_model(model)
-mjx_data = mjx.put_data(model, data)
+
+data.qpos[:6] = np.array([1.5, -1.8, 1.75, -1.25, -1.6, 0])
+
+# mjx_model = mjx.put_model(model)
+# mjx_data = mjx.put_data(model, data)
 
 # jax.config.update('jax_default_matmul_precision', 'high')
 # jax.config.update("jax_enable_x64", True)
